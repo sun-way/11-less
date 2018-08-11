f2d2cf617c7e7ab9d89da6544247b935464f4fdc
sun-way <37132870+sun-way@users.noreply.github.com>
2018-08-11 21:58:22 +0300

    Update OrderList.php

--- OrderList.php
+++ OrderList.php
@@ -6,21 +6,23 @@ namespace example;
 <head>
     <meta charset="utf-8">
 </head>
+<body>
 <?php
 session_start();
 error_reporting(0);
 #error_reporting(E_ALL);
 require_once 'autoload.php';
-if($_POST["form_order"])
+
+if($_POST['form_order'])
 {
     $b = new Order(unserialize($_COOKIE['cook']));
     $b->getOrder();
-    exit();
 }
 if($_POST["clear"])
 {
     Basket::removeProduct();
 }
 ?>
+
 </body>
-</html>
+</h
